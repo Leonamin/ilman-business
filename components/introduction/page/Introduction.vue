8b16ffd062810f5b24cb12c0e578886c5ca65938
Leonamin <yism810@gmail.com>
2024-11-20 18:01:31 +0900

    # ADD - 소개 화면 하단 위아래 애니메이션 화살표 구현

--- components/introduction/page/Introduction.vue
+++ components/introduction/page/Introduction.vue
@@ -9,12 +9,9 @@ const handleClick = () => {
 
 <template>
   <div class="layout">
-
     <IndexHeader/>
     <div class="main-content">
-      <div>
-        <h1 class="animate-content">진료는 그대로<br>매출은 최대로<br>아임파인 일만사업</h1>
-      </div>
+      <h1 class="animate-content">진료는 그대로<br>매출은 최대로<br>아임파인 일만사업</h1>
       <div class="button-list animate-content">
         <SolidButton
             width="150px"
@@ -32,59 +29,55 @@ const handleClick = () => {
         />
       </div>
     </div>
+    <a href="#feature">
+      <div class="arrow-container">
+        <img src="~/assets/svgs/icons/icon_chevron_down.svg" alt="Arrow" class="arrow"/>
+      </div>
+    </a>
   </div>
 </template>
 
 <style scoped>
+/* 레이아웃 */
 .layout {
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  justify-content: start;
   align-items: center;
   width: 100%;
+  height: 100vh;
+
   scroll-behavior: smooth;
 }
 
+
+/* 메인 컨텐츠 */
 .main-content {
   background-color: var(--color-bg-primary);
   display: flex;
-  width: 100%;
-  height: 100vh;
   align-items: center;
-  justify-content: start;
   flex-direction: column;
+  flex-grow: 1;
+  gap: 16px;
 }
 
 .main-content h1 {
   font-size: 4rem;
   font-weight: 700;
   line-height: 1.4;
-  text-align: left;
-  color: #333;
-  font-family: 'Noto Sans KR', sans-serif;
-}
-
-.main-content h5 {
-  font-size: 1.5rem;
-  font-weight: 400;
   text-align: center;
   color: #333;
   font-family: 'Noto Sans KR', sans-serif;
 }
 
-/* 초기 상태 */
-h1, h5 {
+/* 애니메이션 초기 상태 */
+.animate-content {
   opacity: 0;
   transform: translateY(-30px);
-}
-
-/* 애니메이션 활성화 */
-.animate-content {
   animation: fadeIn 1s linear forwards;
-  animation-delay: 1s; /* 1초 뒤에 시작 */
+  animation-delay: 1s;
 }
 
-/* Keyframes 정의 */
 @keyframes fadeIn {
   from {
     opacity: 0;
@@ -96,9 +89,8 @@ h1, h5 {
   }
 }
 
+/* 버튼 리스트 */
 .button-list {
-  opacity: 0;
-  transform: translateY(-30px);
   display: flex;
   flex-direction: row;
   justify-content: center;
@@ -106,6 +98,31 @@ h1, h5 {
   gap: 16px;
 }
 
+/* 화살표 컨테이너 */
+.arrow-container {
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
+  width: 100%; /* 부모 컨테이너 너비에 맞춤 */
+  padding-bottom: 16px; /* 하단 패딩 */
+}
+
+/* 화살표 애니메이션 */
+.arrow {
+  width: 50px;
+  animation: moveUpDown 2s infinite;
+}
+
+@keyframes moveUpDown {
+  0%, 100% {
+    transform: translateY(0);
+  }
+  50% {
+    transform: translateY(-20px);
+  }
+}
+
+/* 반응형 디자인 */
 @media (max-width: 768px) {
   .main-content {
     padding-top: 40px;
@@ -115,9 +132,9 @@ h1, h5 {
     font-size: 2.5rem;
   }
 
-  .main-content h5 {
-    font-size: 1rem;
+  .button-list {
+    flex-direction: column;
+    gap: 12px;
   }
 }
 </style>
-
