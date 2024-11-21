34e822c7890c32c3aa7f1ead100731cdf49184f4
Leonamin <yism810@gmail.com>
2024-11-21 17:09:42 +0900

    # FIX - ScrollToTopButton 수정

--- components/0_components/button/ScrollToTopButton.vue
+++ components/0_components/button/ScrollToTopButton.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 
 // 상단으로 스크롤 이동
+import ChevronUp from "~/components/0_components/icons/ChevronUp.vue";
+
 const scrollToTop = () => {
   window.scrollTo({
     top: 0,
@@ -18,10 +20,7 @@ const scrollToTop = () => {
       class="scroll-to-top"
       @click="scrollToTop"
   >
-    <svg class="chevron-icon" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path fill-rule="evenodd" clip-rule="evenodd"
-            d="M10.5858 9.00009C11.3668 8.21904 12.6332 8.21904 13.4142 9.00009L18.7071 14.293C19.0976 14.6835 19.0976 15.3167 18.7071 15.7072C18.3166 16.0977 17.6834 16.0977 17.2929 15.7072L12 10.4143L6.70711 15.7072C6.31658 16.0977 5.68342 16.0977 5.29289 15.7072C4.90237 15.3167 4.90237 14.6835 5.29289 14.293L10.5858 9.00009Z"/>
-    </svg>
+    <ChevronUp color="white" size="24" />
 
   </button>
 </template>
@@ -43,6 +42,9 @@ const scrollToTop = () => {
   cursor: pointer;
   z-index: 1000;
   transition: opacity 0.3s ease, transform 0.3s ease;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 
 .chevron-icon {
