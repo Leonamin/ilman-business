8fd6eede2740677b9a3d695be23dfa12de7e96a3
Leonamin <yism810@gmail.com>
2024-11-21 17:07:03 +0900

    # EDIT - 의사 인터뷰, 프로세스 수정

--- components/introduction/page/DoctorInterview.vue
+++ components/introduction/page/DoctorInterview.vue
@@ -1,11 +1,39 @@
+<script setup lang="ts">
+</script>
+
 <template>
-  <div id="feature">
-    <h1>실제 사용자들의 인터뷰</h1>
-    <p>이거이거 아주 좋구만 기래</p>
+  <div class="layout">
+    <div class="main-content">
+      <h2>고니를 아냐구요? 그는 제가 아는 최고의 땃쥐였어요</h2>
+    </div>
   </div>
 </template>
 
 <style scoped>
+.layout {
+  padding: 32px 0;
+}
+
+.main-content {
+  width: 100%;
+  max-width: 70%;
+  margin: 0 auto;
+  border-radius: 8px;
+  overflow: hidden;
+}
+
+.main-content h2 {
+  text-align: center;
+  font-size: var(--font-size-h2);
+  font-weight: bold;
+  margin-bottom: 32px;
+}
+
+
+@media (max-width: 768px) {
+  .main-content h2 {
+    font-size: var(--font-size-h2-mobile);
+  }
+}
+
 </style>
-<script setup lang="ts">
-</script>
--- components/introduction/page/WorkProcess.vue
+++ components/introduction/page/WorkProcess.vue
@@ -2,11 +2,48 @@
 </script>
 
 <template>
-  <div>
-    <h1>일처리 트리!</h1>
-    <p>우리 일처리는 요따구다!</p>
+  <div class="layout">
+    <div class="main-content">
+      <h2>우리는 이렇게 일합니다</h2>
+      <h2 class="no-work">네 일을 안한다고 하는 군요 ㅠㅠ</h2>
+    </div>
   </div>
 </template>
 
 <style scoped>
+.layout {
+  padding: 32px 0;
+}
+
+.main-content {
+  width: 100%;
+  max-width: 70%;
+  margin: 0 auto;
+  border-radius: 8px;
+  overflow: hidden;
+}
+
+.main-content h2 {
+  text-align: center;
+  font-size: var(--font-size-h2);
+  font-weight: bold;
+  margin-bottom: 32px;
+}
+
+.no-work {
+  color:transparent;
+}
+
+.no-work:hover {
+  color: red;
+}
+
+
+@media (max-width: 768px) {
+  .main-content h2 {
+    font-size: var(--font-size-h2-mobile);
+  }
+}
+
+
 </style>
