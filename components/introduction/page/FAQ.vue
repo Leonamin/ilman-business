27df0e481961533937fda946235393ebf7331e15
Leonamin <yism810@gmail.com>
2024-11-21 17:01:34 +0900

    # EDIT - FAQ 수정

--- components/introduction/page/FAQ.vue
+++ components/introduction/page/FAQ.vue
@@ -5,19 +5,23 @@ import ChevronUp from "~/components/0_components/icons/ChevronUp.vue";
 
 const faqList: FAQModel[] = [
   {
-    question: "우리 의원에 아임파인을 도입할 수 있을까요?",
+    question: "우리 의원도 설치 가능한가요?",
     answer: "아니요"
   },
   {
-    question: "도입 비용은 얼마나 드나요? 유지 비용은 별도로 발생하나요?",
+    question: "도입 비용은 얼마나 드나요?",
     answer: "100만원입니다."
   },
   {
-    question: "우리 의원에도 도입할 수 있는 규모일까요?",
+    question: "유지 비용도 있나요?",
     answer: "네"
   },
   {
-    question: "도입 후에도 지속적인 지원을 받을 수 있을까요?",
+    question: "하면 뭐가 좋나요?",
+    answer: "위에서 설명했을텐데"
+  },
+  {
+    question: "지속적인 지원도 받을 수 있나요?",
     answer: "돈을 내신다면야."
   }
 ]
@@ -38,16 +42,16 @@ const toggleFAQ = (index: number) => {
   <div class="layout">
     <div class="spacer"></div>
     <div class="faq">
-      <h5>자주 묻는 질문</h5>
+      <h2>자주 묻는 질문</h2>
       <div
           v-for="(item, index) in faqList"
           :key="index"
           class="faq-item"
       >
         <div class="faq-header" @click="toggleFAQ(index)">
-          <h3 :style="{
+          <h5 :style="{
               color: activeIndex === index ? 'var(--color-primary)' : 'black'
-            }">{{ item.question }}</h3>
+            }">{{ item.question }}</h5>
           <ChevronDown
               v-if="activeIndex !== index"
               color="black"
@@ -84,9 +88,9 @@ const toggleFAQ = (index: number) => {
   overflow: hidden;
 }
 
-.faq h5 {
+.faq h2 {
   text-align: center;
-  font-size: var(--font-size-h5);
+  font-size: var(--font-size-h2);
   font-weight: bold;
   margin-bottom: 32px;
 }
@@ -103,6 +107,7 @@ const toggleFAQ = (index: number) => {
   display: flex;
   flex-direction: row;
   justify-content: space-between;
+  align-items: center;
   padding: 16px;
   cursor: pointer;
 }
@@ -111,10 +116,29 @@ const toggleFAQ = (index: number) => {
   background: var(--color-bg-primary-hover)
 }
 
+.faq-header h5 {
+  font-size: var(--font-size-h5);
+  font-weight: bold;
+}
+
 .faq-content {
   padding-inline: 16px;
   line-height: 1.6;
 }
 
+@media (max-width: 768px) {
+  .faq {
+    max-width: 100%;
+  }
+
+  .faq h2 {
+    font-size: var(--font-size-h2-mobile);
+  }
+
+  .faq-header h5 {
+    font-size: var(--font-size-h5-mobile);
+    font-weight: bold;
+  }
+}
 </style>
 
