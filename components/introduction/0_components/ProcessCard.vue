495479ad81a340fe674da95838b6e42c8e76a510
Leonamin <yism810@gmail.com>
2024-11-22 19:17:58 +0900

    # ADD - 일만 사업 절차 카드 순서대로 및 모바일 대응

---
+++ components/introduction/0_components/ProcessCard.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+import type {WorkProcessStepModel} from "~/models/WorkProcessStepModel";
+
+const props = defineProps<{
+  process: WorkProcessStepModel;
+}>();
+
+</script>
+
+<template>
+  <div class="outer card">
+    <div>
+      <p class="text-h5 text-semi-bold text-left spb-small">{{ props.process.title }}</p>
+      <p class="text-body1 text-medium text-left text-positive">{{ props.process.description }}</p>
+    </div>
+    <div class="spacer"></div>
+    <div class="bottom">
+      <div style="width:100px; height:100px; background-color: #cccccc">
+
+      </div>
+    </div>
+<!--    image icon-->
+  </div>
+
+</template>
+
+<style scoped>
+.outer {
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  gap: 16px;
+}
+
+.spacer {
+  flex: 1;
+}
+
+.card {
+  padding: var(--padding-large);
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  border-radius: 16px;
+  aspect-ratio: 1.2;
+}
+
+.bottom {
+  margin-left: auto
+}
+</style>
--- components/introduction/page/WorkProcess.vue
+++ components/introduction/page/WorkProcess.vue
@@ -4,6 +4,7 @@ import type {WorkProcessStepModel} from "~/models/WorkProcessStepModel";
 import ChevronRight from "~/components/0_components/icons/ChevronRight.vue";
 import ChevronDown from "~/components/0_components/icons/ChevronDown.vue";
 import ChevronLeft from "~/components/0_components/icons/ChevronLeft.vue";
+import ProcessCard from "~/components/introduction/0_components/ProcessCard.vue";
 
 const process: WorkProcessStepModel[] = [
   {title: '환자 등록 관리', description: '아임파인이 8개의\n작업을 처리합니다.'},
@@ -14,80 +15,82 @@ const process: WorkProcessStepModel[] = [
   {title: '중간 점검', description: '아임파인이 5개의\n작업을 처리합니다.'},
 ]
 
-const arrows = [
-  {x1: 50, y1: 50, x2: 150, y2: 150}, // 첫 번째 화살표
-  {x1: 150, y1: 150, x2: 250, y2: 250}, // 두 번째 화살표
-];
+const isMobile = ref(false);
+
+// 모바일 화면 768 픽셀 이하인 경우 이미지 변경
+onMounted(() => {
+  window.addEventListener("resize", () => {
+    isMobile.value = window.innerWidth <= 768;
+  });
+});
+
 </script>
 
 <template>
   <h2 class="text-h2 text-semi-bold center-row spb-large">아임파인의 일만사업 절차</h2>
-  <div class="process-outer">
-<!--    첫번째 줄 1, 2, 3-->
-    <div class="process-inner">
-      <div class="process-item flex-equal card">
-        <p class="text-body1 text-semi-bold spb-small">{{ process[0].title }}</p>
-        <p class="text-body2 text-medium text-positive">{{ process[0].description }}</p>
-      </div>
-      <div class="card-gap center-column">
-        <ChevronRight color="black" size="24"/>
-      </div>
-      <div class="process-item flex-equal card">
-        <p class="text-body1 text-semi-bold spb-small">{{ process[1].title }}</p>
-        <p class="text-body2 text-medium text-positive">{{ process[1].description }}</p>
-      </div>
-      <div class="card-gap center-column">
-        <ChevronRight color="black" size="24"/>
-      </div>
-      <div class="process-item flex-equal card">
-        <p class="text-body1 text-semi-bold spb-small">{{ process[2].title }}</p>
-        <p class="text-body2 text-medium text-positive">{{ process[2].description }}</p>
+  <div class="content-layout">
+
+    <div v-if="isMobile">
+      <div class="center-column" v-for="(item, index) in process" :key="item.title">
+        <ProcessCard :process="item"/>
+        <div v-if="index !== process.length - 1" class="card-gap center-column">
+          <ChevronDown color="black" size="24"/>
+        </div>
       </div>
     </div>
-<!--    공백 줄 오직 가장 왼쪽 아래 화살표를 위해 존재-->
-    <div class="process-inner">
-      <div class="flex-equal"></div>
-      <div class="card-gap"/>
-      <div class="flex-equal"></div>
-      <div class="card-gap"/>
-      <div class="flex-equal card-gap center-column">
-        <ChevronDown color="black" size="24"/>
-      </div>
-    </div>
-<!--    세번째 줄 6, 5, 4-->
-    <div class="process-inner">
-      <div class="process-item flex-equal card">
-        <p class="text-body1 text-semi-bold spb-small">{{ process[5].title }}</p>
-        <p class="text-body2 text-medium text-positive">{{ process[5].description }}</p>
-      </div>
-      <div class="card-gap center-column">
-        <ChevronLeft color="black" size="24"/>
-      </div>
-      <div class="process-item flex-equal card">
-        <p class="text-body1 text-semi-bold spb-small">{{ process[4].title }}</p>
-        <p class="text-body2 text-medium text-positive">{{ process[4].description }}</p>
-      </div>
-      <div class="card-gap center-column">
-        <ChevronLeft color="black" size="24"/>
-      </div>
-      <div class="process-item flex-equal card">
-        <p class="text-body1 text-semi-bold spb-small">{{ process[3].title }}</p>
-        <p class="text-body2 text-medium text-positive">{{ process[3].description }}</p>
+    <div v-if="!isMobile">
+      <div class="process-outer">
+        <!--    첫번째 줄 1, 2, 3-->
+        <div class="process-inner">
+          <ProcessCard class="flex-equal" :process="process[0]"/>
+          <div class="card-gap center-column">
+            <ChevronRight color="black" size="24"/>
+          </div>
+          <ProcessCard class="flex-equal" :process="process[1]"/>
+          <div class="card-gap center-column">
+            <ChevronRight color="black" size="24"/>
+          </div>
+          <ProcessCard class="flex-equal" :process="process[2]"/>
+
+        </div>
+        <!--    공백 줄 오직 가장 왼쪽 아래 화살표를 위해 존재-->
+        <div class="process-inner">
+          <div class="flex-equal"></div>
+          <div class="card-gap"/>
+          <div class="flex-equal"></div>
+          <div class="card-gap"/>
+          <div class="flex-equal card-gap center-column">
+            <ChevronDown color="black" size="24"/>
+          </div>
+        </div>
+        <!--    세번째 줄 6, 5, 4-->
+        <div class="process-inner">
+          <ProcessCard class="flex-equal" :process="process[5]"/>
+          <div class="card-gap center-column">
+            <ChevronLeft color="black" size="24"/>
+          </div>
+          <ProcessCard class="flex-equal" :process="process[4]"/>
+          <div class="card-gap center-column">
+            <ChevronLeft color="black" size="24"/>
+          </div>
+          <ProcessCard class="flex-equal" :process="process[3]"/>
+        </div>
       </div>
     </div>
   </div>
 
-  <!--    <div class="process-grid">-->
-  <!--      <div class="process-item" v-for="(step, index) in process" :key="index">-->
-  <!--        <p class="text-body1 text-semi-bold spb-small">{{ step.title }}</p>-->
-  <!--        <p class="text-body2 text-medium text-positive">{{ step.description }}</p>-->
-  <!--      </div>-->
-  <!--    </div>-->
 
 </template>
 
 
 <style scoped>
+.content-layout {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
 .process-outer {
   display: flex;
   flex-direction: column;
@@ -108,28 +111,23 @@ const arrows = [
   flex: 1;
 }
 
-.process-item {
-  padding: var(--padding-large);
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-start;
-  align-items: flex-start;
-}
-
-.card {
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-  border-radius: 16px;
-  aspect-ratio: 1.2;
-}
-
 .card-gap {
   width: 54px;
   height: 54px;
 }
 
 @media (max-width: 768px) {
-  .process-grid {
-    grid-template-columns: repeat(2, 1fr); /* 2열로 변경 */
+  .content-layout {
+    padding-inline: var(--padding-medium);
+  }
+
+  .process-outer {
+    padding-inline: var(--padding-medium);
+
+  }
+
+  .process-inner {
+    flex-direction: column;
   }
 }
 </style>
