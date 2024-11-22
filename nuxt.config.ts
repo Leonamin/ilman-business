608452befc1b7b5b4f6b32290132ae560614b010
Leonamin <yism810@gmail.com>
2024-11-22 12:00:49 +0900

    # FIX - vercel 빌드 안되는 문제

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     compatibilityDate: '2024-04-03',
+    build: {
+        transpile: ['ant-design-vue', 'lodash-es'],
+    },
     devtools: {enabled: true},
     vite: {
         resolve: {
