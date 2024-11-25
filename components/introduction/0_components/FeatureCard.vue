33a2ffe62fe77816f012e7eb60aeae00f2903cf1
Leonamin <yism810@gmail.com>
2024-11-25 14:47:55 +0900

    # ADD - 기능 카드 호버링 시 글자 표시 구현

--- components/introduction/0_components/FeatureCard.vue
+++ components/introduction/0_components/FeatureCard.vue
@@ -1,49 +1,27 @@
 <script setup lang="ts">
 
 import type {FeatureModel} from "~/models/FeatureModel";
-import {type CardPreset, cardPresets} from "~/style/button/card/CardPreset";
 
 const props = defineProps<{
   feature: FeatureModel;
-  preset?: CardPreset;
 }>();
-
-const cssCardStyle = [
-  'card',
-  'outer'
-]
-const cardStyle = computed(() => cardPresets[props.preset || 'white']);
 
 </script>
 
 <template>
-  <div :class="[...cssCardStyle, cardStyle]">
+  <div class="card">
     <div>
-      <p class="text-h5 text-semi-bold text-left spb-small text-line-break text-line-height-small">
+      <p class="title text-semi-bold text-left spb-small text-line-break text-line-height-small">
         {{ props.feature.title }}</p>
-      <p class="text-body1 text-medium text-left text-positive text-line-break text-line-height-small">
+      <p class="description text-medium text-left text-positive text-line-break text-line-height-small">
         {{ props.feature.description }}</p>
     </div>
     <div class="spacer"></div>
-    <div class="bottom">
-      <div style="width:100px; height:100px; background-color: #cccccc">
-
-      </div>
-    </div>
-    <!--    image icon-->
   </div>
 
 </template>
 
 <style scoped>
-.outer {
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-start;
-  align-items: flex-start;
-  gap: 16px;
-}
-
 .spacer {
   flex: 1;
 }
@@ -52,9 +30,38 @@ const cardStyle = computed(() => cardPresets[props.preset || 'white']);
   padding: var(--padding-large);
   border-radius: 16px;
   aspect-ratio: 1.2;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  gap: 16px;
+  overflow: hidden;
+  border: solid 1px var(--color-border-primary);
 }
 
 .bottom {
   margin-left: auto
+}
+
+.title {
+  font-size: var(--font-size-h3);
+  white-space: pre-line;
+  word-break: keep-all;
+  word-wrap: break-word;
+  transition: visibility 0s, font-size 0.5s;
+}
+
+.description {
+  opacity: 0;
+  font-size: var(--font-size-body1);
+  transition: opacity 0.5s;
+}
+
+.card:hover .title {
+  font-size: var(--font-size-h5);
+}
+
+.card:hover .description {
+  opacity: 1;
 }
 </style>
--- components/introduction/page/Feature.vue
+++ components/introduction/page/Feature.vue
@@ -8,17 +8,17 @@ const features: FeatureModel[] = [
   {
     title: "도입 단 한달부터\n상승하는 매출을 직접 확인하세요.",
     description: "일만사업에 참여하면 단 한달만에 상승하는 매출을 직접 확인할 수 있습니다.\n\n아임파인은 일만사업에 참여하는 병원들을 위해 매출 상승을 위한 다양한 서비스를 제공합니다.",
-    preset: "green"
+    image: "~/assets/images/background/feature/background-money.jpg"
   },
   {
     title: "까다로운 환자관리\n저희가 전부 도와드리겠습니다.",
     description: "일만사업에 참여하면 까다로운 환자관리를 저희가 전부 도와드립니다.\n\n아임파인은 일만사업에 참여하는 병원들을 위해 환자관리를 위한 다양한 서비스를 제공합니다.",
-    preset: "blue"
+    image: "~/assets/images/background/feature/background-laptop.jpg"
   },
   {
     title: "필요한 자료\n저희는 이미 준비를 끝내놓았습니다.",
     description: "일만사업에 참여하면 필요한 자료를 저희가 이미 준비를 끝내놓았습니다.\n\n아임파인은 일만사업에 참여하는 병원들을 위해 필요한 자료를 제공합니다.",
-    preset: "yellow"
+    image: "~/assets/images/background/feature/background-document.jpg"
   }
 ]
 </script>
@@ -28,7 +28,8 @@ const features: FeatureModel[] = [
     <div class="explanation spb-large">
       <AnimatedElement>
         <h2 class="spb-large text-h2 text-bold">일만사업이란?</h2>
-        <h5 class="spb-large text-h5 text-medium ">일차병원 만성질환관리사업(이하 일만사업)은 만성질환(고혈압/당뇨병)을 가진 환자들의 건강 수명 증진을 위한 국민건강보험 공단의 사업입니다.</h5>
+        <h5 class="spb-large text-h5 text-medium ">일차병원 만성질환관리사업(이하 일만사업)은 만성질환(고혈압/당뇨병)을 가진 환자들의 건강 수명 증진을 위한 국민건강보험
+          공단의 사업입니다.</h5>
       </AnimatedElement>
 
       <SolidButton preset="primaryOutline" text="일만사업에 대해 더 알아보기"/>
@@ -40,9 +41,9 @@ const features: FeatureModel[] = [
       </AnimatedElement>
       <AnimatedElement :threshold=0.1>
         <div class="card-container spb-large">
-          <div class="flex-equal" v-for="feature in features" :key="feature.title">
-            <FeatureCard :feature="feature" :preset="feature.preset"/>
-          </div>
+          <FeatureCard
+              :feature="feature" :preset="feature.preset" class="flex-equal" v-for="feature in features"
+              :key="feature.title"/>
         </div>
       </AnimatedElement>
 
@@ -93,7 +94,7 @@ const features: FeatureModel[] = [
   display: flex;
   flex-direction: row;
   justify-content: center;
-  align-items: center;
+  align-items: stretch;
   width: 100%;
   gap: var(--padding-large);
 }
