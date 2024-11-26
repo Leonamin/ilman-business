423ed445674489fd600993b7d9e8a0cc7adf51e9
Leonamin <yism810@gmail.com>
2024-11-26 12:00:50 +0900

    # ADD - 버튼 변경

--- style/button/ButtonColorPreset.ts
+++ style/button/ButtonColorPreset.ts
@@ -1,8 +1,10 @@
-export type ButtonPreset = 'primary' | 'primaryOutline' | 'secondary';
+export type ButtonPreset = 'primary' | 'primaryOutline' | 'secondary' | 'blue' | 'blueOutline';
 
 // 프리셋을 CSS 클래스 이름과 연결
 export const buttonClassMap: Record<ButtonPreset, string> = {
     primary: 'button-primary',
     primaryOutline: 'button-primary-outline',
-    secondary: 'button-secondary'
+    secondary: 'button-secondary',
+    blue: 'button-blue',
+    blueOutline: 'button-blue-outline'
 };
