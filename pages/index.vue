54d99f19dc22018cd1622ac754aab3245e923df3
Leonamin <yism810@gmail.com>
2024-12-04 12:30:40 +0900

    # EDIT - 파일 이름 구조 정리

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,11 @@
 <script setup lang="ts">
-import Introduction from "~/src/2_view/1_index/1_section/IntroductionSection.vue";
-import Feature from "~/src/2_view/1_index/1_section/FeatureSection.vue";
-import WorkProcess from "~/src/2_view/1_index/1_section/WorkProcessSection.vue";
-import FAQ from "~/src/2_view/1_index/1_section/FAQSection.vue";
-import DoctorInterview from "~/src/2_view/1_index/1_section/DoctorInterviewSection.vue";
-import ProfitCalculator from "~/src/2_view/1_index/1_section/ProfitCalculatorSection.vue";
-import Explanation from "~/src/2_view/1_index/1_section/ExplanationSection.vue";
-import SolidButton from "~/src/2_view/0_components/button/SolidButton.vue";
+import Introduction from "~/src/2_view/1_index/1_section/1_IntroductionSection.vue";
+import Feature from "~/src/2_view/1_index/1_section/3_FeatureSection.vue";
+import WorkProcess from "~/src/2_view/1_index/1_section/5_WorkProcessSection.vue";
+import FAQ from "~/src/2_view/1_index/1_section/8_FAQSection.vue";
+import DoctorInterview from "~/src/2_view/1_index/1_section/6_DoctorInterviewSection.vue";
+import ProfitCalculator from "~/src/2_view/1_index/1_section/4_ProfitCalculatorSection.vue";
+import Explanation from "~/src/2_view/1_index/1_section/2_ExplanationSection.vue";
 import FixedContactButton from "~/src/2_view/1_index/0_component/FixedContactButton.vue";
 
 definePageMeta({
