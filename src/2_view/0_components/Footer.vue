db02b065086130e2e8d00024473738cff2d0e35e
Leonamin <yism810@gmail.com>
2024-12-05 19:20:23 +0900

    # EDIT - 푸터 수정

--- src/2_view/0_components/Footer.vue
+++ src/2_view/0_components/Footer.vue
@@ -3,16 +3,16 @@
     <div class="content text-tertiary">
       <div class="footer-top">
         <div class="terms">
-          <a class="text-body1 text-semi-bold" href="https://caresquare.notion.site/150a1c7601908099aba3c688b9235c17?pvs=4">개인정보처리방침</a>
+          <a class="text-body2 text-semi-bold" href="https://caresquare.notion.site/150a1c7601908099aba3c688b9235c17?pvs=4">개인정보처리방침</a>
           <span class="text-subtitle3 text-bold text-secondary">주식회사 케어스퀘어</span>
         </div>
         <div class="row-start text-line-break">
-          <a class="text-body1 text-semi-bold" href="tel:02-6952-3078">문의하기 02-6952-3078</a>
+          <a class="text-body2 text-semi-bold" href="tel:02-6952-3078">문의하기 02-6952-3078</a>
           <div class="spl-24"/>
-          <span class="text-body1 ">(평일 오전 10시 ~ 오후 7시)</span>
+          <span class="text-body2 text-normal">(평일 오전 10시 ~ 오후 7시)</span>
         </div>
       </div>
-      <div class="footer-bottom text-body2 text-line-break ">
+      <div class="footer-bottom text-caption2 text-normal">
         <span>서울특별시 강남구 역삼로 156, 6층</span>
         <span>대표이사: 오병엽</span>
         <span>사업자번호:449-86-01362</span>
